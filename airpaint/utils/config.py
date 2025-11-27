90d2a4ab6900c8e65a8ea61bce12990025371943
frandom13 <francisco.m.v.domingos@gmail.com>
2025-11-27 19:13:13 +0000

    atualizacao_config_+cores_+spray_mode

--- airpaint/utils/config.py
+++ airpaint/utils/config.py
@@ -8,8 +8,13 @@ erase_thickness = 20
 colors = [
     (0, 255, 0),   
     (255, 0, 0),   
-    (0, 0, 255),  
+    (0, 0, 255),
+    (0, 255, 255), 
+    (255, 0, 255), 
+    (255, 255, 255)
 ]
 
 color_index = 0
 current_color = colors[color_index]
+
+spray_mode = False
