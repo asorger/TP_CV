0105a01de12f0e2f5ac97e6d4d527f914b1a2a08
frandom13 <francisco.m.v.domingos@gmail.com>
2025-12-01 18:38:44 +0000

    New Feature Historico e Guardar

--- airpaint/utils/config.py
+++ airpaint/utils/config.py
@@ -4,6 +4,8 @@ canvas = None
 
 thickness = 10
 erase_thickness = 40
+history = []
+MAX_HISTORY = 20
 
 colors = [
     (0, 255, 0),   
@@ -18,3 +20,8 @@ color_index = 0
 current_color = colors[color_index]
 
 spray_mode = False
+
+def save_state(canvas):
+    if len(history) >= MAX_HISTORY:
+        history.pop(0)
+    history.append(canvas.copy())
